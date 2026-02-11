5ec666b3221044f400857fa49eba6e6766e3e7a2
MicroChipCookies <code_vee@fastmail.com>
2026-02-10 21:21:01 -0500

    AlertsButton component created

---
+++ frontend/src/components/AlertsButton.tsx
@@ -0,0 +1,43 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Alert, Linking, Pressable, StyleSheet, View } from "react-native";
+
+function openAlerts() {
+
+    try {
+        Linking.openURL("https://www.corvallisoregon.gov/news?field_microsite_tid=581");
+    } catch(error) {
+        Alert.alert('Sorry. Unable to open alerts at this time.')
+    }
+    
+}
+
+function AlertsButton() {
+
+    return(
+        <View style={styles.alertsContainer}>
+            <Pressable onPress={openAlerts}>
+                <Ionicons name="warning-outline" style={styles.alertsIcon} />
+            </Pressable>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    alertsIcon: {
+        fontSize: 36,
+    },
+    alertsContainer: {
+        backgroundColor: "yellow",
+        color: 'red',
+        width: 50,
+        height: 50,
+        borderRadius: 12,
+        position: 'absolute',
+        bottom: 120,
+        right: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
+
+export default AlertsButton;
