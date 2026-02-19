dee232091ae2cf28a96b6ce2569222fec005f8cc
Jamie Murphy <hello@itsjamie.dev>
2026-02-18 19:52:04 -0800

    cache: Add cache middleware

--- backend/src/beaver_bus/api.ts
+++ backend/src/beaver_bus/api.ts
@@ -1,16 +1,21 @@
 import express from "express"
 
 import APIClient from "./client";
+import CacheMiddleware from "../cache"
 
 const router = express.Router();
 
 const BeavBusAPI = new APIClient();
 
-router.get('/stops', async (req, res) => {
+router.get('/stops', CacheMiddleware({
+    timeout: 60 * 60 * 1000 * 24
+}), async (req, res) => {
     res.send(await BeavBusAPI.getStops(req.query.route_id));
 })
 
-router.get('/routes', async (req, res) => {
+router.get('/routes', CacheMiddleware({
+    timeout: 60 * 60 * 1000 * 24
+}), async (req, res) => {
     res.send(await BeavBusAPI.getRoutes())
 })
 
