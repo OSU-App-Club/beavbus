055a55b37997ad97e32cbf0df9c193e062594cf0
SeanG <70712778+SeanG-rsd@users.noreply.github.com>
2026-04-22 19:34:10 -0700

    got some work done!

--- frontend/src/scripts/onSearch.ts
+++ frontend/src/scripts/onSearch.ts
@@ -1,6 +1,6 @@
 import Fuse from 'fuse.js'
 
-export default function onSearch(routes: string[], searchText: string) {
+export function onSearch(routes: string[], searchText: string) {
     const fuse = new Fuse(routes);
     const results = fuse.search(searchText)
     var items: string[] = []
@@ -10,4 +10,20 @@ export default function onSearch(routes: string[], searchText: string) {
     }
 
     return items;
+}
+
+export async function getLocations(searchText: string) {
+
+    const METHOD = "geocoding"
+    const QUERY = "Oregon State University"
+    const PARAMS = "proximity=ip"
+    const KEY = "fCIJiQ6Td63CAgVetC3q"
+
+    const response = await fetch(`https://api.maptiler.com/${METHOD}/${QUERY}.json?key=${KEY}`)
+
+    const result = await response.json();
+
+    console.log(result)
+
+    return 0;
 }
